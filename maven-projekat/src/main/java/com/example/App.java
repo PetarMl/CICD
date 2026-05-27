04b0f75f04262ddbdb1e1f2a77ebe5ced1b3adfc
PetarMl <petar.mladenov@pmf.edu.rs>
2026-05-27 03:18:41 +0200

    Maven app v1

---
+++ maven-projekat/src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String server = "Ubuntu Maven Server";
+
+        System.out.println("================================");
+        System.out.println("      CI/CD Pipeline Status     ");
+        System.out.println("================================");
+
+        System.out.println("Server  : " + server);
+        System.out.println("Status  : ONLINE");
+        System.out.println("Build   : SUCCESS");
+
+        System.out.println("================================");
+    }
+}
