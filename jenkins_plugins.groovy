d0aa14523a9b7fab90f92b80e36322bf401b1e46
PetarMl <petar.mladenov@pmf.edu.rs>
2026-05-27 16:02:47 +0200

    pro

--- jenkins_plugins.groovy
+++ jenkins_plugins.groovy
@@ -4,7 +4,7 @@ pm=Jenkins.instance.pluginManager
 uc=Jenkins.instance.UpdateCenter
 
 uc.updateAllSites()
-["github","docker-workflow","prometheus","ansible"].each{
+["github","docker-workflow","prometheus","ansible","maven"].each{
     if(![pm.getPlugin(it)]){
         deployment=uc.getPlugin(it).deploy(true)
         deployment.get()
